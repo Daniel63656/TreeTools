bfe83ad66eb0777ae53bd13fb7174c65c1cd5610
Daniel <d.maier020@gmail.com>
2022-04-07 17:05:10 +0200

    addad functions for HashStack

--- TraVerS/src/main/java/com/immersive/collection/HashStack.java
+++ TraVerS/src/main/java/com/immersive/collection/HashStack.java
@@ -3,13 +3,24 @@ package com.immersive.collection;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class HashStack<K, V extends Comparable<V>> extends HashMap<K, ArrayList<V>> {
+public class HashStack<K, V> extends HashMap<K, ArrayList<V>> {
 
-    public V getNthStackEntry(K key, int index) {
-        return super.get(key).get(index);
+    public V getStackEntry(K key, int index) {
+        if (!containsKey(key))
+            return null;
+        return get(key).get(index);
     }
 
-    public int putValue(K key, V value) {
+    public V getStackTop(K key) {
+        if (!containsKey(key))
+            return null;
+        ArrayList<V> stack = get(key);
+        if (stack.isEmpty())
+            return null;
+        return stack.get(stack.size()-1);
+    }
+
+    public int pushValue(K key, V value) {
         if (!this.containsKey(key)) {
             ArrayList<V> stack = new ArrayList<>();
             this.put(key, stack);
@@ -18,14 +29,44 @@ public class HashStack<K, V extends Comparable<V>> extends HashMap<K, ArrayList<
         }
         ArrayList<V> stack = get(key);
         stack.add(value);
-        return 0;
+        return stack.size()-1;
     }
 
-    public void removeValue(K key, V value) {
+    public V popValue(K key) {
+        if (!this.containsKey(key))
+            return null;
+        ArrayList<V> stack = get(key);
+        V value = stack.get(stack.size()-1);
+        stack.remove(stack.size()-1);
+        return value;
+    }
+
+    public void removeValueFromStack(K key, V value) {
         if (!this.containsKey(key)) {
             return;
         }
         ArrayList<V> stack = get(key);
         stack.remove(value);
+    }
+
+    public int getStackSize(K key) {
+        if (!containsKey(key))
+            return 0;
+        return get(key).size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        for (ArrayList<V> stack : values()) {
+            if (!stack.isEmpty())
+                return false;
+        }
+        return true;
+    }
+
+    public boolean isStackEmpty(K key) {
+        if (!containsKey(key))
+            return true;
+        return get(key).isEmpty();
     }
 }
